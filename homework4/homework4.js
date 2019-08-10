27541b36fa3a2fb63dae96ab57b6e21b6d451897
mikhail771 <mmihajlov771@gmail.com>
2019-08-10 17:26:31 +0300

    Homework #4

--- homework4/homework4.js
+++ homework4/homework4.js
@@ -5,6 +5,22 @@ let operator = null;
 let operand1 = null;
 let operand2 = null;
 
+function add(a,b) {
+    return Number(a) + Number(b);
+}
+
+function sub(a,b) {
+    return a - b;
+}
+
+function mult(a,b) {
+    return a * b;
+}
+
+function div(a,b) {
+    return a / b;
+}
+
 while (true) {
     operator = prompt('Выберите оператор: add, sub, mult, div');
 
@@ -19,27 +35,28 @@ do {
     operand1 = prompt('Введите первый операнд');
 }
 while (isNaN(operand1));
+
 do {
     operand2 = prompt('Введите второй операнд');
 }
 while (isNaN(operand2));
 
-function calculate(a,b) {
     switch (operator) {
         case 'add':
-            return Number(a) + Number(b);
+           result = add(operand1, operand2);
+           break;
 
         case 'sub':
-            return a - b;
+            result = sub(operand1, operand2);
+            break;
 
         case 'mult':
-            return a * b;
+            result = mult(operand1, operand2);
+            break;
 
         case 'div':
-            return a / b;
-    }
+            result = div(operand1, operand2);
+            break;
 }
-
-result = calculate(operand1, operand2);
 
 alert(result);
