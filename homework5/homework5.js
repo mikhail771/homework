07302a16b001671055c4fc5fd4c5554c972b5461
mikhail771 <mmihajlov771@gmail.com>
2019-08-12 23:25:17 +0300

    Homework #5 + refactoring "Bonus"

--- homework5/homework5.js
+++ homework5/homework5.js
@@ -12,25 +12,30 @@ function replaceAll(str, oldChar, newChar) {
     return index < 0 ? newString : replaceAll(newString, oldChar, newChar);
 }
 
+function isCharOne(a) {
+    if(a != 1)alert('Можно вводить только один символ');
+}
+
 do {
     string = prompt('Введите строку');
 }
 while (string.length < 1);
 
 do {
-    oldChar = prompt('Введите символ, который хотите заменить')
+    oldChar = prompt('Введите символ, который хотите заменить');
+    isCharOne(oldChar.length);
 }
 while (oldChar.length !=1);
 
-do {
-    newChar = prompt('Введите символ, на который заменить ' + oldChar)
-}
-while (newChar.length !=1);
-
 if (string.indexOf(oldChar) != -1) {
+
+    do {
+        newChar = prompt('Введите символ, на который заменить ' + oldChar);
+        isCharOne(newChar.length);
+    }
+    while (newChar.length !=1);
+
     replaceAll(string,oldChar,newChar);
     alert(newString);
 }
 else alert('Символ ' + oldChar + ' не найден в строке ' + string);
-
-
