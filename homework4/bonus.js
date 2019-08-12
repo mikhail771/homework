b5a30c4981ee82c194b4a0ecab90eec18b92629d
mikhail771 <mmihajlov771@gmail.com>
2019-08-12 16:53:37 +0300

    Homework #4 - Bonus

---
+++ homework4/bonus.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let number = null;
+let oneMore = false;
+
+function multiTable() {
+    do {
+        number = prompt('Введите число');
+    }
+    while (number > 10 || number < 0 || isNaN(number));
+
+    for (let i = 0; i < 11; i++) {
+        console.log(number + '*' + i + "=" + number * i);
+    }
+}
+
+multiTable();
+
+do {
+    oneMore = confirm("Хотите еще?");
+    if(oneMore){
+        multiTable();
+    }
+} while (oneMore);
+
+
