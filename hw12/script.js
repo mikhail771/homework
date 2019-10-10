f37f5d03a99786b13b36eee6d8edf6d64c0d9321
mikhail771 <mmihajlov771@gmail.com>
2019-10-10 16:53:00 +0300

    Homework #16

--- hw12/script.js
+++ hw12/script.js
@@ -1,47 +1,47 @@
 'use strict';
-const SMALL_BURGER = {
-    price: 50,
-    cal: 20
-};
 
-const BIG_BURGER = {
-    price: 100,
-    cal: 40
-};
-
-const CHEESE = {
-    price: 10,
-    cal: 20
-};
-
-const SALAD = {
-    price: 20,
-    cal: 5
-};
-
-const POTATOES = {
-    price: 15,
-    cal: 5
-};
-
-const CONDIMENT = {
-    price: 15,
-    cal: 0
-};
-
-const MAYO = {
-    price: 20,
-    cal: 5
-};
 
 function Burger(burger) {
     this.totalPrice = burger.price;
     this.totalCalories = burger.cal;
+    const SMALL_BURGER = {
+        price: 50,
+        cal: 20
+    };
 
+    const BIG_BURGER = {
+        price: 100,
+        cal: 40
+    };
+
+    const CHEESE = {
+        price: 10,
+        cal: 20
+    };
+
+    const SALAD = {
+        price: 20,
+        cal: 5
+    };
+
+    const POTATOES = {
+        price: 15,
+        cal: 5
+    };
+
+    const CONDIMENT = {
+        price: 15,
+        cal: 0
+    };
+
+    const MAYO = {
+        price: 20,
+        cal: 5
+    };
 }
 Burger.prototype.addTopping = function (addition) {
-    recalculateTotalPrice(this, addition);
-    recalculateTotalCalories(this, addition);
+    this.recalculateTotalPrice(addition);
+    this.recalculateTotalCalories(this, addition);
 };
 
 Burger.prototype.setTotalPrice = function (price) {
@@ -60,26 +60,27 @@ Burger.prototype.getTotalCalories = function () {
     return this.totalCalories;
 };
 
-function recalculateTotalPrice(burger, addition) {
-    const oldPrice = burger.getTotalPrice();
-    const newPrice = oldPrice + addition.price;
-    burger.setTotalPrice(newPrice);
+Burger.prototype.recalculateTotalPrice = function (addition) {
+    const oldPrice = this.getTotalPrice();
+    const newPrice = oldPrice + this.addition.price;
+    this.setTotalPrice(newPrice);
+};
+
+Burger.prototype.recalculateTotalCalories = function (addition) {
+    const oldCalories = this.getTotalCalories();
+    const newCalories = oldCalories + this. addition.cal;
+    this.setTotalCalories(newCalories);
 }
 
-function recalculateTotalCalories(burger, addition) {
-    const oldCalories = burger.getTotalCalories();
-    const newCalories = oldCalories + addition.cal;
-    burger.setTotalCalories(newCalories);
-}
 
-let burger = new Burger(SMALL_BURGER);
+let burger = new Burger(this.SMALL_BURGER);
 console.log(burger.totalCalories, burger.totalPrice);
 
-burger.addTopping(CHEESE);
+burger.addTopping(this.CHEESE);
 console.log(burger.totalCalories, burger.totalPrice);
 
-burger.addTopping(SALAD);
+burger.addTopping(this.SALAD);
 console.log(burger.totalCalories, burger.totalPrice);
 
-burger.addTopping(CONDIMENT);
+burger.addTopping(this.CONDIMENT);
 console.log(burger.totalCalories, burger.totalPrice);
---
+++ hw16/scripts.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const prom = fetch('https://jsonplaceholder.typicode.com/photos?_limit=50');
+const gallery = document.getElementById('all-photos');
+const photo = document.getElementsByTagName('IMG');
+const darkTheme = document.getElementById('dark-side');
+
+function hangEventShowOne() {
+    for(let i = 0; i < photo.length; i++) {
+        photo[i].addEventListener('click', showOnePhoto);
+    }
+}
+
+function buildGallery(data) {
+    data.forEach(function (item) {
+        const img = document.createElement("img");
+        const li = document.createElement("li");
+
+        img.setAttribute('src', item.url);
+        gallery.appendChild(li);
+        li.appendChild(img);
+        hangEventShowOne();
+    })
+}
+
+function createCloseBtn() {
+    const span = document.createElement("SPAN");
+    const x = document.createTextNode("\u00D7");
+
+    span.id = "close";
+    span.appendChild(x);
+    span.addEventListener('click', hidePhoto);
+
+    return span;
+}
+
+function showOnePhoto() {
+    this.classList.add('show');
+    makeDarkness();
+    this.parentElement.appendChild(createCloseBtn());
+}
+
+function makeDarkness() {
+    darkTheme.style.display = "block";
+}
+
+function hidePhoto() {
+    darkTheme.style.display = "none";
+    this.previousSibling.classList.remove('show');
+    this.remove();
+}
+
+prom.then((resp) => {
+    resp.json().then((data) => {
+        buildGallery(data);
+    })
+});
