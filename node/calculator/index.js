00552fde1ab44bc74bc3b12c9ddfda3c077a3211
mikhail771 <mmihajlov771@gmail.com>
2019-11-08 10:25:01 +0200

    Homework #17

---
+++ node/calculator/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function add(a,b) {
+    return a + b;
+}
+
+function sub(a,b) {
+    return a - b;
+}
+
+function mult(a,b) {
+    return a * b;
+}
+
+function div(a,b) {
+    return a / b;
+}
+
+module.exports = {
+    sub,
+    mult,
+    div,
+    add
+};
